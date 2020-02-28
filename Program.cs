9eb767f618fdbf921626a9a8f35169f273387b5f
mikayla_mendez@ymail.com <mikayla_mendez@ymail.com>
2020-02-28 16:41:28 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, my name is Mikayla Anne Mendez!");
+        }
+    }
+}
